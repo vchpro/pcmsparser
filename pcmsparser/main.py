e63ed580b83ab4ee7f1fac18f1da90641694ca19
Влад <mail.vchpro@yandex.ru>
2022-06-09 11:16:08 +0300

    fix format

--- pcmsparser/main.py
+++ pcmsparser/main.py
@@ -62,7 +62,7 @@ def build_view(tasks):
     keys = ['Решено у', 'Контест', 'Задача', 'Ссылка']
 
     for task in tasks:
-        e.append([str(task[0]).center(10), task[1], task[2], task[3]])
+        e.append([str(task[0]).center(10) if len(str(task[0])) == 2 else str(task[0]).center(11), task[1], task[2], task[3]])
 
     print(tabulate(e, headers=keys, tablefmt='fancy_grid', stralign='center'))
 
